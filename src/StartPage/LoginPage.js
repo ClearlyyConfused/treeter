d223ba52279257e2462af22441c0c6ab0ea98205
Louis Doan <louisdoan9@gmail.com>
2022-07-18 17:20:19 -0400

    Have forms send info to api for registering/login

--- src/StartPage/LoginPage.js
+++ src/StartPage/LoginPage.js
@@ -1,7 +1,51 @@
+import { useState } from 'react';
+
 function LoginPage({ setCurrentPage }) {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const req = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ username: username, password: password }),
+		};
+		fetch('https://treetter.herokuapp.com/login', req)
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.error) {
+					setError(data.error);
+				} else {
+					localStorage.setItem('token', data.token);
+					setCurrentPage('StartPage');
+				}
+			});
+	}
+
 	return (
 		<div>
 			<h1>Login</h1>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="username">Username:</label>
+				<input
+					type="text"
+					name="username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<label htmlFor="password">Password:</label>
+				<input
+					type="text"
+					name="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+			{error}
 			<button onClick={() => setCurrentPage('StartPage')}>Back</button>
 		</div>
 	);
--- src/StartPage/RegisterPage.js
+++ src/StartPage/RegisterPage.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+
 function RegisterPage({ setCurrentPage }) {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const req = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ username: username, password: password }),
+		};
+		fetch('https://treetter.herokuapp.com/register', req);
+		setCurrentPage('LoginPage');
+	}
+
 	return (
 		<div>
 			<h1>Register</h1>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="username">Username:</label>
+				<input
+					type="text"
+					name="username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<label htmlFor="password">Password:</label>
+				<input
+					type="text"
+					name="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
 			<button onClick={() => setCurrentPage('StartPage')}>Back</button>
 		</div>
 	);
