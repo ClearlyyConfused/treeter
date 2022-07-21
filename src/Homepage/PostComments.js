a248e96afcff1904cead4b064a05cbf434175e0d
Louis Doan <louisdoan9@gmail.com>
2022-07-20 21:56:37 -0400

    Adjust css styles

--- src/Homepage/PostComments.js
+++ src/Homepage/PostComments.js
@@ -1,9 +1,12 @@
 import commentIcon from '../images/chat.svg';
+import commentIcon2 from '../images/chat-outline.svg';
 
 function PostComments({ postId, getPosts, postComments }) {
+	const commentIconSrc = postComments.length > 0 ? commentIcon : commentIcon2;
+
 	return (
 		<a href={postId}>
-			<img className="like-icon" src={commentIcon} alt="Like" />
+			<img className="like-icon" src={commentIconSrc} alt="Like" />
 			<p>{postComments.length}</p>
 		</a>
 	);
--- src/Post/Post.js
+++ src/Post/Post.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import PageSidebar from '../Homepage/PageSidebar';
 import CommentForm from './CommentForm';
+import PostLikes from '../Homepage/PostLikes';
 import './post.css';
 
 function Post() {
@@ -40,13 +41,25 @@ function Post() {
 					<div>Loading...</div>
 				) : (
 					<div>
-						<h3>{postInfo.author}</h3>
-						<p>{postInfo.content}</p>
-						<CommentForm postId={postId} getComments={getComments} />
-						<div>
+						<div className="comment-header">
+							<div className="comment-title">
+								<h3>{postInfo.author}</h3>
+								<p>on {postInfo.timestamp}</p>
+							</div>
+							<p>{postInfo.content}</p>
+						</div>
+						<div className="comment-footer">
+							<CommentForm postId={postId} getComments={getComments} />
+							<PostLikes
+								postId={postId}
+								getPosts={getComments}
+								postLikes={postInfo.likes}
+							/>
+						</div>
+						<div className="post-comments">
 							{postInfo.comments.map((comment) => {
 								return (
-									<div className="post-comments">
+									<div className="post-comment">
 										<div className="comment-title">
 											<h4>{comment.author}</h4>
 											<p>on {comment.timestamp}</p>
