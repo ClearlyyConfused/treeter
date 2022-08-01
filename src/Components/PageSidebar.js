2d377fe7aca608b20d2dd39b1769d48e601579e5
Louis Doan <louisdoan9@gmail.com>
2022-07-31 20:59:14 -0400

    Fix display at smaller viewports

--- src/Components/PageSidebar.js
+++ src/Components/PageSidebar.js
@@ -6,13 +6,15 @@ function PageSidebar() {
 
 	return (
 		<div className="homepage-sidebar">
-			<h2>Welcome {localStorage.getItem('username')}</h2>
-			<a href="/">
-				<button>HOME</button>
-			</a>
-			<a href="/">
-				<button onClick={logout}>LOGOUT</button>
-			</a>
+			<h2>{localStorage.getItem('username')}</h2>
+			<div className="sidebar-buttons">
+				<a href="/">
+					<button>HOME</button>
+				</a>
+				<a href="/">
+					<button onClick={logout}>LOGOUT</button>
+				</a>
+			</div>
 		</div>
 	);
 }
