a86565a96a6ed1a74f2bae5701b930d1d8742249
Louis Doan <louisdoan9@gmail.com>
2023-07-17 18:28:49 -0400

    Add error message if username already exists

--- src/Auth/RegisterPage.js
+++ src/Auth/RegisterPage.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 function RegisterPage({ setCurrentPage }) {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
 
 	function handleSubmit(e) {
 		e.preventDefault();
@@ -10,8 +11,25 @@ function RegisterPage({ setCurrentPage }) {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ username: username, password: password }),
-		});
-		setCurrentPage('LoginPage');
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.error) {
+					setError(data.error);
+				} else {
+					fetch('https://treeter-api.vercel.app/login', {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({ username: username, password: password }),
+					})
+						.then((response) => response.json())
+						.then((data) => {
+							localStorage.setItem('token', data.token);
+							localStorage.setItem('username', data.username);
+							window.location.reload();
+						});
+				}
+			});
 	}
 
 	return (
@@ -21,15 +39,11 @@ function RegisterPage({ setCurrentPage }) {
 				<label htmlFor="username">Username:</label>
 				<input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
 				<label htmlFor="password">Password:</label>
-				<input
-					type="password"
-					name="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
+				<input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 				<button type="submit">Submit</button>
 			</form>
 			<button onClick={() => setCurrentPage('StartPage')}>Back</button>
+			<p className="error-message">{error}</p>
 		</div>
 	);
 }
