32b7f4e1eb3a94118aa68d211d320f72f4ad186e
Louis Doan <louisdoan9@gmail.com>
2024-05-22 23:03:36 -0400

    Add tests for posting/deleting replies

--- src/Testing/endToEnd.test.js
+++ src/Testing/endToEnd.test.js
@@ -231,6 +231,70 @@ describe('Test posting functionality of the site', () => {
 		expect(treet).toBe(content);
 	});
 
+	test('Replying to a "Treet" updates the reply section with the reply', async () => {
+		const content = randomString(20);
+		await driver.wait(until.elementLocated(By.className('homepage-post'))).then(async (e) => {
+			await e.click();
+		});
+		await driver
+			.wait(until.elementLocated(By.xpath('//textarea[@placeholder="Treet your reply!"]')))
+			.then(async (e) => {
+				await e.sendKeys(content);
+				await driver.findElement(By.xpath('//button[text()="Comment"]')).then(async (e) => {
+					await e.click();
+				});
+			});
+		await driver.wait(
+			until.elementTextIs(driver.findElement(By.xpath('//textarea[@placeholder="Treet your reply!"]')), '')
+		);
+		const reply = await driver
+			.wait(until.elementLocated(By.className('comment-content')))
+			.then(async (e) => await e.findElement(By.xpath('.//p')).then((e2) => e2.getText()));
+		expect(reply).toBe(content);
+	});
+});
+
+describe('Test deleting functionality of the site', () => {
+	let driver;
+	beforeEach(async () => {
+		driver = await new Builder().forBrowser(Browser.CHROME).build();
+		await driver.get('http://localhost:3000/');
+		await driver.manage().window().maximize();
+		// login with test account
+		await driver.wait(until.elementLocated(By.xpath('//button[text()="LOGIN"]'))).then(async (e) => {
+			await e.click();
+		});
+		await driver.wait(until.elementLocated(By.name('username'))).then(async (e) => {
+			await e.sendKeys('Test');
+		});
+		await driver.wait(until.elementLocated(By.name('password'))).then(async (e) => {
+			await e.sendKeys('testPassword');
+		});
+		await driver.wait(until.elementLocated(By.xpath('//button[text()="Submit"]'))).then(async (e) => {
+			await e.click();
+		});
+		await driver.wait(until.elementLocated(By.css('h2'))).then((e) => e.getText());
+	});
+	afterEach(async () => {
+		await driver.quit();
+	});
+
+	test('Deleting a reply displays a "Unable to find Treet" message', async () => {
+		await driver.wait(until.elementLocated(By.className('homepage-post'))).then(async (e) => {
+			await e.click();
+		});
+		await driver.wait(until.elementLocated(By.className('post-comment'))).then(async (e) => {
+			await e.click();
+		});
+		await driver.wait(until.elementLocated(By.className('delete-button'))).then(async (e) => {
+			await e.click();
+		});
+		const message = await driver
+			.wait(until.elementLocated(By.className('no-treet')))
+			.then((e) => e.getText());
+		expect(message).toBe('Unable to find Treet');
+	});
+
 	test('Deleting a "Treet" displays a "Unable to find Treet" message', async () => {
 		await driver.wait(until.elementLocated(By.className('homepage-post'))).then(async (e) => {
 			await e.click();
